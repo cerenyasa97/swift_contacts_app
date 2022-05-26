c69ae38a68c9d4c6d80914a7e52cfb01e3735e6f
Ceren Yaşa <cerenyasa97@hotmail.com>
2022-05-26 23:18:24 +0300

    Initial Commit

---
+++ ceren_yasa_week_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ceren_yasa_week_2
+//
+//  Created by Ceren Yaşa on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
